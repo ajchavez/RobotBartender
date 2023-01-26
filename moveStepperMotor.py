18ffb4421323e22cb92f138d689018308aeec20c
ajchavez <ajchavez@mtu.edu>
2023-01-25 18:19:24 -1000

    test servo

--- moveStepperMotor.py
+++ moveStepperMotor.py
@@ -1,41 +1,39 @@
 from TMC2209_Raspberry_Pi.src.TMC_2209.TMC_2209_StepperDriver import *
 
+def my_callback(channel):
+    pass
 
-class StepperMotor():
-    def __init__(self):
-        self.tmc = TMC_2209(21,16,20)
+def moveStepper(steps):
+    tmc = TMC_2209(21,16,20)
+    tmc.set_loglevel(Loglevel.DEBUG)
+    tmc.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
+    tmc.set_direction_reg(False)
+    tmc.set_current(300)
+    tmc.set_interpolation(True)
+    tmc.set_spreadcycle(False)
+    tmc.set_microstepping_resolution(2)
+    tmc.set_internal_rsense(False)
+    tmc.readIOIN()
+    tmc.readCHOPCONF()
+    tmc.readDRVSTATUS()
+    tmc.readGCONF()
+    tmc.set_acceleration(2000)
+    tmc.set_max_speed(500)
+    tmc.set_stallguard_callback(26,80,my_callback)
         
-    def setup(self):
-        self.tmc.set_loglevel(Loglevel.DEBUG)
-        self.tmc.set_movement_abs_rel(MovementAbsRel.ABSOLUTE)
-        self.tmc.set_direction_reg(False)
-        self.tmc.set_current(300)
-        self.tmc.set_interpolation(True)
-        self.tmc.set_spreadcycle(False)
-        self.tmc.set_microstepping_resolution(2)
-        self.tmc.set_internal_rsense(False)
-        self.tmc.readIOIN()
-        self.tmc.readCHOPCONF()
-        self.tmc.readDRVSTATUS()
-        self.tmc.readGCONF()
-        self.tmc.set_acceleration(2000)
-        self.tmc.set_max_speed(500)
-        self.tmc.set_stallguard_callback(26,80,self.my_callback)
-
-    def my_callback(self, channel):
-        self.tmc.stop()
-        
-    def move(self, steps):
-        return self.tmc.run_to_position_steps(steps, MovementAbsRel.RELATIVE)
-        
-    def kill(self):
-        self.tmc.set_motor_enabled(False)
-        self.tmc.deinit()
-        
+    tmc.set_motor_enabled(True)
+    tmc.run_to_position_steps(steps, MovementAbsRel.RELATIVE) 
+    tmc.set_motor_enabled(False)
+    tmc.deinit()
+def triggerServo():
+    from adafruit_servokit import ServoKit
+    kit = ServoKit(channels=16)
+    kit.servo[7].angle = 180
         
 if __name__ == "__main__":
-    stepper = StepperMotor()
-    stepper.setup()
-    print(stepper.move(1000))
-    stepper.kill()
+    #import sys
+    #print(sys.argv[1])
+    #stepper = moveStepper(int(sys.argv[1]))
+    triggerServo()
+    #stepper = moveStepper(-1000)
         
